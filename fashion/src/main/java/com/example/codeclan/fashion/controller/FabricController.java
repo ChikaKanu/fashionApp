4eb9a2b53c861f0807593e923f92cae8c83105b7
ChikaKanu <chika.kanu@procompletions.com>
2019-05-04 18:56:20 +0100

    measurement controller complete

--- fashion/src/main/java/com/example/codeclan/fashion/controller/FabricController.java
+++ fashion/src/main/java/com/example/codeclan/fashion/controller/FabricController.java
@@ -1,4 +1,23 @@
 package com.example.codeclan.fashion.controller;
 
+import com.example.codeclan.fashion.models.Fabric;
+import com.example.codeclan.fashion.repository.fabrics.FabricRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fabrics")
 public class FabricController {
+    @Autowired
+    FabricRepository fabricRepository;
+
+
+    @PutMapping(value = "/{id}")
+    public void updateAddFabric(@PathVariable long id) {
+        Fabric fabric = fabricRepository.getOne(id);
+        fabricRepository.save(fabric);
+    }
 }
--- fashion/src/main/java/com/example/codeclan/fashion/controller/MeasurementController.java
+++ fashion/src/main/java/com/example/codeclan/fashion/controller/MeasurementController.java
@@ -1,4 +1,23 @@
 package com.example.codeclan.fashion.controller;
 
+import com.example.codeclan.fashion.models.Measurement;
+import com.example.codeclan.fashion.repository.measurements.MeasurementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/measurements")
 public class MeasurementController {
+
+    @Autowired
+    MeasurementRepository measurementRepository;
+
+    @PutMapping(value = "/{id}")
+    public void updateAddMeasurement(@PathVariable long id) {
+        Measurement measurement = measurementRepository.getOne(id);
+        measurementRepository.save(measurement);
+    }
 }
