a647b412b9706bc2e4fb8fd581e05255afdb6942
ChikaKanu <chika.kanu@procompletions.com>
2019-05-04 19:03:26 +0100

    controllers complete

--- fashion/src/main/java/com/example/codeclan/fashion/controller/TailorController.java
+++ fashion/src/main/java/com/example/codeclan/fashion/controller/TailorController.java
@@ -1,4 +1,24 @@
 package com.example.codeclan.fashion.controller;
 
-public class TailorController {
+import com.example.codeclan.fashion.models.Tailor;
+import com.example.codeclan.fashion.repository.tailors.TailorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tailors")
+    public class TailorController {
+
+    @Autowired
+    TailorRepository tailorRepository;
+
+    @PutMapping(value = "/{id}")
+    public void updateAddTailor(@PathVariable long id) {
+        Tailor tailor = tailorRepository.getOne(id);
+        tailorRepository.save(tailor);
+    }
+
 }
