cd02af22414b01c6dd13826c626f12d77c71f7b6
ChikaKanu <chika.kanu@procompletions.com>
2019-05-04 18:59:53 +0100

    style controller complete

--- fashion/src/main/java/com/example/codeclan/fashion/controller/StyleController.java
+++ fashion/src/main/java/com/example/codeclan/fashion/controller/StyleController.java
@@ -1,4 +1,23 @@
 package com.example.codeclan.fashion.controller;
 
+import com.example.codeclan.fashion.models.Style;
+import com.example.codeclan.fashion.repository.styles.StyleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/styles")
 public class StyleController {
+
+    @Autowired
+    StyleRepository styleRepository;
+
+    @PutMapping(value = "/{id}")
+    public void updateAddStyle(@PathVariable long id) {
+        Style style = styleRepository.getOne(id);
+        styleRepository.save(style);
+    }
 }
