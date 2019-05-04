860e24c001c155874a682978c9232322152b632a
ChikaKanu <chika.kanu@procompletions.com>
2019-05-04 18:48:56 +0100

    customer controller complete

--- fashion/src/main/java/com/example/codeclan/fashion/controller/CustomerController.java
+++ fashion/src/main/java/com/example/codeclan/fashion/controller/CustomerController.java
@@ -1,4 +1,23 @@
 package com.example.codeclan.fashion.controller;
 
+import com.example.codeclan.fashion.models.Customer;
+import com.example.codeclan.fashion.repository.customers.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @PutMapping(value = "/{id}")
+    public void updateAddCustomer(@PathVariable long id) {
+        Customer customer = customerRepository.getOne(id);
+        customerRepository.save(customer);
+    }
 }
